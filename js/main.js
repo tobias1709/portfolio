e41d5abbd9f85a38e1a29069098f84bd1aaed7cd
Tobias Kolstrup <5884577@RTS-365.dk>
2019-08-26 13:20:38 +0200

    dropdown

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('connected');
+	document.querySelector('.work').addEventListener('mouseover', () => {
+		alert('totally functional js dropdown');
+	});
+});
